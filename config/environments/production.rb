3e19bbd53f6252adf3ce23a40bb158ce7c645ed5
Jonas Preisler <heypreisler@gmail.com>
2022-03-24 15:26:01 +0800

    f

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  config.hosts << "wo09.hatchboxapp.com"
 end
